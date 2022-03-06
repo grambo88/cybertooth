1e1666bd614a6773f0b2fe3e467c7f79a641bcc4
grambo88 <gosnopro@gmail.com>
2022-03-06 11:24:02 -0500

    testing

---
+++ ty.py
@@ -0,0 +1,21 @@
+from modifiedMain1 import Webdriver
+from time import sleep
+import csv
+
+
+browser = Webdriver()
+
+sleep(2)
+
+
+browser.start_process()
+
+# browser.remove_scratched()
+
+# browser.clean_data()
+
+print('complete')
+
+
+
+
