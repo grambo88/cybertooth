021ea3ee96ab18a3f4dcf27094c7d6ffb24cbdf1
grambo88 <gosnopro@gmail.com>
2022-07-18 07:04:57 -0400

    updates

--- cbv2.py
+++ cbv2.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.select import Select
 from time import sleep
 import itertools
+import sys
 import csv, re
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -17,30 +18,56 @@ class Webdriver:
 
     def __init__(self):
         self.generateBroswer()
-        sleep(1)
+        # sleep(1)
         # self.table_scrape()
-        sleep(1)
+        # sleep(1)
         pd.set_option('display.max_rows', None, 'display.max_columns', None)
-
 
 
     def generateBroswer(self): # sets all driver options and creates the instance
         print('*** STARTING  : generateBrowser() method ***')
         chrome_options = webdriver.ChromeOptions()
-        prefs = {"profile.default_content_setting_values.notifications": 2}
-        chrome_options.add_experimental_option("prefs", prefs)
-        chrome_options.add_experimental_option("detach", True)
-        chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+        # prefs = {"profile.default_content_setting_values.notifications": 2}
+        # chrome_options.add_experimental_option("prefs", prefs)
+        # chrome_options.add_experimental_option("detach", True)
+        # chrome_options.add_argument('--disable-blink-features=AutomationControlled')
         chrome_options.add_argument("--start-maximized")
         self.driver = webdriver.Chrome(executable_path="/usr/local/bin/chromedriver", options=chrome_options)
-        url = "https://entries.horseracingnation.com/entries-results/belmont-park/2022-05-15"
+        url_base = "https://entries.horseracingnation.com/entries-results/belmont-park/"
+        url_date = "2022-05-15"
+        url = url_base+url_date
         self.driver.get(url)
-        sleep(5)
+        sleep(1)
+        # print('*** Removing Pop-up ***')
         self.driver.refresh()
-        sleep(2)
+        sleep(1)
+        # self.driver.switchTo().parentFrame()
         print('*** COMPLETED : generateBrowser() method ***')
+        sleep(1)
         # WebDriverWait(self.driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.CSS_SELECTOR,"iframeCssSelector")))
 
+    def master(self):
+
+        filename = 'extra/Belmont-Dates-2022.csv'
+        with open(filename, 'r') as csvfile:
+            datareader = csv.reader(csvfile)
+            for row in datareader:
+                print(row)
+                # row = row.text
+                # str(row)
+                row1 = join(row)
+                url_base = "https://entries.horseracingnation.com/entries-results/belmont-park/"
+                url = url_base + row1
+                self.driver.get(url)
+                sleep(5)
+                print('got to here')
+
+
+
+    def next_page(self):
+        # url_base_2 = "2022-05-14"
+        self.driver.get("https://entries.horseracingnation.com/entries-results/belmont-park/2022-05-14")
+        self.get_num_tables()
 
 
     def get_num_tables(self): # returns 27 (num of tables on page)
@@ -52,39 +79,28 @@ class Webdriver:
 
     def get_payouts(self): # returns payouts table
         web_pay_df = pd.read_html(self.driver.find_element_by_xpath("//table[@class='table table-hrn table-payouts']").get_attribute('outerHTML'))[0]
-        web_pay_df.to_csv('_data_files/payouts.csv')
+        web_pay_df.to_csv('data/payouts.csv')
 
         return web_pay_df
 
 
     def get_entries(self): # returns entries table
         web_entries_df = pd.read_html(self.driver.find_element_by_xpath("//table[@class='table table-sm table-hrn table-entries']").get_attribute('outerHTML'))[0]
-        web_entries_df.to_csv('_data_files/entries.csv')
+        web_entries_df.to_csv('data/entries.csv')
 
         return web_entries_df
 
 
     def get_exotics(self): # returns exotics table
         web_exotics_df = pd.read_html(self.driver.find_element_by_xpath("//table[@class='table table-hrn table-exotic-payouts']").get_attribute('outerHTML'))[0]
-        web_exotics_df.to_csv('_data_files/exotics.csv')
+        web_exotics_df.to_csv('data/exotics.csv')
 
         return web_exotics_df
 
 
-    def get_num_races(self): # returns number of races
-        num_tables = self.driver.find_elements_by_tag_name('table')
-        race_count = len(num_tables) / 3
-        # print(race_count)
-
-        return int(race_count)
-
-
     def get_scratched(self): # returns scratched class
         scratched = self.driver.find_elements_by_xpath("//*[@class='scratched']")
-        # print('\n')
-        # print(scratched)
-        for s in scratched:
-            # print(s.text)
+        for s in scratched: # only prints one line now, needs to print all scratchers
 
             return s.text
 
@@ -93,7 +109,7 @@ class Webdriver:
         num_tables = self.driver.find_elements_by_tag_name('table')
         race_count = len(num_tables) / 3
 
-        return race_count
+        return int(race_count)
 
 
     def get_headers(self): # returns 'PP Horse / Sire Trainer / Jockey ML'
@@ -102,63 +118,119 @@ class Webdriver:
         return col_headers.text 
 
 
-    def show_races(self):
-        pass
+    def num_of_races(self):
+        data_sets = self.driver.find_elements_by_class_name('my-5')
+        x = len(data_sets)
+
+        return x
+
+
+    def get_race_info(self):
+
+        r_header = self.driver.find_elements_by_xpath("//*[@class='race-header']")
+        dist = self.driver.find_elements_by_xpath("//*[@class='col-lg-auto flex-grow-1 race-distance']")
+        purse = self.driver.find_elements_by_xpath("//*[@class='col-lg-auto race-purse']")
+        path = 'data/race_info.csv'
+        sys.stdout = open(path, 'a') # maybe use writerow feature?
+        for (a, b, c) in zip(r_header, dist, purse):
+             print (a.text + ',', b.text + ',', c.text)
+
+
+    def new_purse(self):
+        purse = self.driver.find_elements_by_xpath("//*[@class='col-lg-auto race-purse']")
+        comma = ','
+        for i in purse:
+            for x in i.text:
+                x.replace(',', '')
+        print(i)
+        
 
 
     def new_process(self):
 
         # col-lg-auto race-purse
+
         data_sets = self.driver.find_elements_by_class_name('my-5')
+        r_info = self.driver.find_elements_by_xpath("//*[@class='col-lg-auto flex-grow-1 race-distance']")
+        time = self.driver.find_elements_by_class_name('race-time')
 
-        for x in range(0,len(data_sets)):
-            purse = self.driver.find_elements_by_xpath("//*[@class='row']")
-            for y in purse:
-                # print(y.text)
+        dist = None
+        # post = []
+        new_list = []
 
-                fullstring = 'Belmont Park Race'
+        for dist in r_info:
+            print(dist.text)
+            dist = dist.text
 
-            
-                if fullstring != None and y.text == fullstring:
+        y = 0
 
-                    print('found race')
-                    print(y.text)
-                else:
-                    print('found junk')
-                    print(y.text)
-                # print('Found ' + len())
+        # for dist in r_info:
+        #     # print(dist.text)
+        #     y = y + 1
+        #     for post in time:
+
+        #         # dist = [dist]   ---------->>>>>
+        #         print(dist, post)
+
+        for post in time:
+            # print(post.text) # keep 
+            post = [post.text]
+            post.append(dist)
+            print(post)
 
 
 
-        race_info_df = data_sets[5]
 
-        # purse = self.driver.find_elements_by_xpath("//*[@class='row']")
+        print('dist object is type: ')
 
-        # purse = data_sets.find_element_by_class_name('col-lg-auto race-purse')
-        
+        print(type(dist))
 
-        # for x in purse:
-        #     print(x.text)
+        print('post object is type: ')
 
-        # race_info_df = pd.read_html(self.driver.find_element_by_xpath())
-        # race_info_df.to_csv('_data_files/race_info_df.csv')
+        print(type(post))
 
-        print(race_info_df) # returns web element
+        print("END")
 
-        c = 0
-        for x in data_sets: # returns num of races 
-           c = c + 1
-           print('data: ', c)
-           # print(len(c))
+        # for i in dist & post:
+        #     new_list = dist[i] + post[i]
+        #     print(new_list)
+
+        # for i in range(len(dist)):
+        #     dist[i].extend(post[i])
+
+        # return dist
+
+       # return new_list
+
+        # for _ in range(0,len(data_sets)):
+
+
+        #     constructed_race = [dist.text]
+        #     constructed_race.append(post.text)
+
+        #     print(constructed_race)
+
+        # print(len(data_sets))
+
+        # # for x in range(0,len(data_sets)):
+        # rows = self.driver.find_elements_by_xpath("//*[@class='row']")
+        # test = self.driver.find_elements_by_xpath("//*[@class='col-lg-auto flex-grow-1 race-distance']")
+        # purse = self.driver.find_elements_by_xpath("//*[@class='col-lg-auto race-purse']")
+
+
+        # c = 0
+        # for x in data_sets: # returns num of races 
+        #    c = c + 1
+        #    print('data: ', c)
+
 
     def regroup_data(self):
 
-            file = open('_data_files/horse_stats1.csv', 'r')
+            file = open('data/horse_stats.csv', 'r')
             reader = csv.reader(file)
-            new_list = open('_data_files/stuff.csv', 'w')
+            new_list = open('data/stuff.csv', 'w')
             writer = csv.writer(new_list)
             lists_from_csv = []
-            # for x in range(0,4):
 
             for row in reader:
                 lists_from_csv.append(row)
@@ -168,16 +240,6 @@ class Webdriver:
             for x in lists_from_csv:
                 print(x)
 
-            # with open('_data_files/stuff.csv') as f:
-            #     for line1,line2 in itertools.zip_longest(*[f]*2):
-            #         print(line1,line2)
-
-
-                # for row[2] in reader:
-                #     print("dont know")
-
-
-
 
     def table_scrape(self):
 
@@ -186,65 +248,45 @@ class Webdriver:
         num_tables = self.driver.find_elements_by_tag_name('table')
         race_count = len(num_tables) / 3
 
-        for x in range(1, int(int(race_count)+1)):
-            print('Race: ', x)
+        # for x in range(1, int(int(race_count)+1)):
+        #     print('Race: ', x)
 
         tables = self.driver.find_elements_by_xpath("//table[@class='table table-sm table-hrn table-entries']")
-        print('################################')
-        print('there are ', len(tables), ' entries_tables')
-        print('################################')
 
         scratched = self.driver.find_elements_by_xpath("//*[@class='scratched']")
-        print('\n')
-        print(scratched)
-        for s in scratched:
-            print(s.text)
-
         # print('\n')
+        # print(scratched)
+        # for s in scratched:
+        #     print(s.text)
 
         col_headers = self.driver.find_element_by_tag_name('thead')
-        print(col_headers.text) # prints 'PP Horse / Sire Trainer / Jockey ML'
+        # print(col_headers.text) # prints 'PP Horse / Sire Trainer / Jockey ML'
 
         n = 0
 
         for race in tables:
 
-            n = n+1
-            # print('Race: ', n)
-
-            # col_headers = self.driver.find_element_by_tag_name('thead')
-            # print(col_headers.text) # prints 'PP Horse / Sire Trainer / Jockey ML'
-            
+            n = n+1            
             for entry in race.find_elements_by_tag_name('tr'):
-                
+
                 if entry not in scratched:
+                    # print('got here')
+
                     horse_stats = []
                     for stat in entry.find_elements_by_tag_name('td'):
                         horse_stats.append(stat.text)
 
                     if horse_stats == []:
                         pass
-                        # print('first')
+
                     else:
                         addRaceNum = 'Race:' + str(n)
                         horse_stats.insert(0, addRaceNum) # need to add Track and Date, hash value?
+                        path = 'data/horse_stats.csv'
+                        sys.stdout = open(path, 'a')
+                        print(horse_stats) # , newline=""
 
-                        # ('_data_files/horse_stats.csv')
-                        print(horse_stats)
-                        with open('_data_files/horse_stats1.csv', 'a', newline="") as entrie_csv:
-                            writer = csv.writer(entrie_csv)
-                            writer.writerow(horse_stats) # this works but splits each into 3 rows
-                            
-
-
-            # print('################################')
-
-        sleep(1)
-
-
-        print('*** COMPLETED : table_scrape() method ***')
-
-
+        # print(horse_stats.text)
 
 
 
--- main.py
+++ main.py
@@ -1,11 +1,33 @@
-from cbv2 import Webdriver
+from cbv2 import Webdriver # this actually runs the script generateBrowser
+
 from time import sleep
 import csv
 
 
 browser = Webdriver()
+ 
+# sleep(2)
+# for i in range(0,2):
+# 	browser = 'browser' + str(i)
+# 	browser = Webdriver()
+# 	sleep(2)
+# 	(browser+str(i)).get_num_races()
+# 	sleep(10)
+# 	browser2.get_payouts()
+# 	sleep(10)
 
-sleep(2)
+
+# trackName = "Belmont-Dates-2022.csv"
+
+# with open(trackName, 'r') as fin:
+# 	reader = csv.reader(fin)
+# 	for row in reader:
+# 		print(row)
+
+
+
+
+
 
 # browser()
 # browser.get_num_tables()
